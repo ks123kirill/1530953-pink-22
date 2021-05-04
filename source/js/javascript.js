3764d9f595ea286f434d488541af40673b59dd0d
ks123kirill <ks123kirill@gmail.com>
2021-05-04 13:20:31 +0300

    Доработал код после первого review

--- source/js/javascript.js
+++ source/js/javascript.js
@@ -1,19 +1,25 @@
 let mainNavJs = function () {
   let navMain = document.querySelector('.main-nav');
   let navToggle = document.querySelector('.main-nav__toggle');
-  // let mainHeader = documement.querySelector('.main-header');
-  // let mainHeaderOpacity = document.querySelector('.main-header--opacity');
+  let mainHeader = document.querySelector('.main-header');
+  let logo = document.querySelector('.logo');
 
-  navMain.classList.remove('main-nav--nojs');
-  // mainHeader.classList.add('main-header--opacity');
+  navMain.classList.remove('main-nav--no-js');
+  navMain.classList.add('main-nav--open');
+  mainHeader.classList.add('main-header--js');
+  mainHeader.classList.add('main-header--opacity');
+  logo.classList.remove('logo--boxshadow');
 
   navToggle.addEventListener('click', function() {
-    if (navMain.classList.contains('main-nav--closed')) {
-      navMain.classList.remove('main-nav--closed');
-      navMain.classList.add('main-nav--opened');
+    logo.classList.toggle('logo--boxshadow');
+    mainHeader.classList.toggle('main-header--opacity');
+
+    if (navMain.classList.contains('main-nav--close')) {
+      navMain.classList.remove('main-nav--close');
+      navMain.classList.add('main-nav--open');
     } else {
-      navMain.classList.add('main-nav--closed');
-      navMain.classList.remove('main-nav--opened');
+      navMain.classList.add('main-nav--close');
+      navMain.classList.remove('main-nav--open');
     }
   });
 }
