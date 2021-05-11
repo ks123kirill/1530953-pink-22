5d3d0d5761bd6b184b69da37cc01ce478a0926e2
ks123kirill <ks123kirill@gmail.com>
2021-05-11 15:43:23 +0300

    добавил min.css и min.js в build

--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,7 @@ const styles = () => {
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(less())
+    .pipe(gulp.dest("build/css"))
     .pipe(postcss([
       autoprefixer(),
       csso()
@@ -45,6 +46,7 @@ const html = () => {
 
 const scripts = () => {
   return gulp.src("source/js/script.js")
+    .pipe(gulp.dest("build/js"))
     .pipe(terser())
     .pipe(rename("script.min.js"))
     .pipe(gulp.dest("build/js"))
